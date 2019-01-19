0ac81e85a3171bc4440e36601b3b95c3abba276e
Sena <senaaltinkaynak.be@gmail.com>
2019-01-19 12:43:35 -0500

    write add method

---
+++ src/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+
+    public static double add(double num1, double num2){
+        return num1 + num2;
+    }
+    public static double substract(double num1, double num2){
+        return num1 - num2;
+    }
+
+}
